a6c99d219ecfce6094fc28ab5bf6997b06f0157a
Billiam <billiamthesecond@gmail.com>
2014-06-21 21:08:18 -0500

    Adding status echos to shell provisioner.

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -9,9 +9,11 @@ chruby 2.1.2
 sudo chmod -R ug+rwX $PUPPET_DIR
 
 if [ `gem query --local | grep librarian-puppet | wc -l` -eq 0 ]; then
+  echo 'Installing librarian puppet'
   gem install librarian-puppet --no-ri --no-rdoc
   cd $PUPPET_DIR && librarian-puppet install --clean
 else
+  echo 'Updating librarian puppet'
   cd $PUPPET_DIR && librarian-puppet update
 fi
 
