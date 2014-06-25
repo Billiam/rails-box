d8c6033b7bc8fc355c74b40995672e85a1bf597d
Billiam <billiamthesecond@gmail.com>
2014-06-25 01:10:59 -0500

    Adding terminal color, tmux init to bashrc.

--- puppet/private/site/manifests/bash.pp
+++ puppet/private/site/manifests/bash.pp
@@ -2,4 +2,10 @@ class site::bash {
   file { '/home/vagrant/.bash_aliases':
     content => template("site/bash_aliases.erb"),
   }
+
+  file_line { 'bash color terminal':
+    path => '/home/vagrant/.bashrc',
+    line => '    screen-256color|linux|cygwin|xterm-color) color_prompt=yes;;',
+    match => '.*xterm-color.*\) color_prompt=yes;;'
+  }
 }
--- puppet/private/site/manifests/tmux.pp
+++ puppet/private/site/manifests/tmux.pp
@@ -2,5 +2,12 @@ class site::tmux {
   tmux::config {'tmux':
     target => '/home/vagrant/.tmux.conf',
     owner => 'vagrant',
+    source => 'puppet:///modules/site/tmux'
+  }
+
+  file_line { 'tmux-bashrc':
+    path => '/home/vagrant/.bashrc',
+    line => 'test -z "$TMUX" && (tmux attach || tmux new-session)',
+    match => '.*tmux attach.*'
   }
 }
